4b215261f125b0d3fde6a6129ec0177341c07cc1
Maryna Longnickel <37321032+MarynaLongnickel@users.noreply.github.com>
2018-11-03 15:05:50 -0400

    Update test.js

--- test.js
+++ test.js
@@ -82,7 +82,7 @@ get the prediction
 */
 function predict(imgData) {
         
-        var class_names = ['Foodz','_Not_foodz_']
+        var class_names = ['Foodz','Not_foodz']
         //get the prediction 
         var pred = model.predict(preprocess(imgData)).dataSync()
         console.log(pred)            
@@ -109,7 +109,7 @@ async function start(){
 	//img = document.getElementById('image').files[0];
 	
         
-        model = await tf.loadModel('model/model.json')
+        model = await tf.loadModel('model.json')
         
         var status = document.getElementById('status')
       
